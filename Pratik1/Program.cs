4b35f150d80014df70a2e81dfb6f664753bf1c39
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2024-09-30 06:17:20 +0300

    yorum satırında düzeltme

--- Pratik1/Program.cs
+++ Pratik1/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Ahmet Fikri YILDIZ");// burda adım ve soyadımı yazdım.
 
-Console.Write("Patika'nin uzman kadrosu ve deneyimli egitmenleri sayesinde yazilim dünyasina hizli bir sekilde giris yapabilecegim için bu bootcampteyim."); // burda projede bulunma sebebimi yazdım.
+Console.Write("Patika'nin uzman kadrosu ve deneyimli egitmenleri sayesinde yazilim dünyasina hizli bir sekilde giris yapabilecegim için bu bootcampteyim."); // burda bootcamp'te bulunma sebebimi yazdım.
 
 Console.ReadLine(); //kodun consoledaki çıktısı daha güzel gözüksün diye bir kod ekledim.
