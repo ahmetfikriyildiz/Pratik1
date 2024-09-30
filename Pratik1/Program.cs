669198b2715e9a5c767b84b4e06949f805e9ed14
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2024-09-30 06:11:08 +0300

    Add project files.

---
+++ Pratik1/Program.cs
@@ -0,0 +1,4 @@
+﻿
+Console.WriteLine("Ahmet Fikri YILDIZ");
+Console.Write("Patika'nin uzman kadrosu ve deneyimli egitmenleri sayesinde yazilim dünyasina hizli bir sekilde giris yapabilecegim için bu bootcampteyim.");
+Console.ReadLine();
